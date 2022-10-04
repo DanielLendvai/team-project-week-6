ab44a278b307a8e075a6be7681a389964be45d8d
Errorcode07 <bbotond0829@gmail.com>
2022-10-04 10:29:57 +0300

    .

--- frontend/script.js
+++ frontend/script.js
@@ -3,18 +3,18 @@ const rootElement = document.querySelector("#root");
 rootElement.insertAdjacentHTML(
   "afterend",
   `<div class="container">
-<div class="nav">
-<a href="">
-<div class="hamburger-icon"></div>
-<div class="hamburger-icon"></div>
-<div class="hamburger-icon"></div></a>
-<a href="">apartments</a><a href="">why griffith wood</a><a href="">living here</a><a href="">LOGO</a><a href="">find us</a><a href="">enquire now</a>
-</div>
-<div class="titleText">
-<h1>A new way of city living</h1>
-<p>A different avenue of renting</p>
-</div>
-</div>`
+    <div class="nav">
+    <a href="">
+    <div class="hamburger-icon"></div>
+        <div class="hamburger-icon"></div>
+    <div class="hamburger-icon"></div></a>
+    <a href="">apartments</a><a href="">why griffith wood</a><a href="">living here</a><a href="">LOGO</a><a href="">find us</a><a href="">enquire now</a>
+    </div>
+    <div class="titleText">
+    <h1>A new way of city living</h1>
+    <p>A different avenue of renting</p>
+    </div>
+    </div>`
 );
 
 const containerElement = document.querySelector(".container");
