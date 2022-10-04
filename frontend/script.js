9485767eb357a1c89d4a57d362ac4850e6040cc5
Daniel Lendvai <lendvai.daniel92@gmail.com>
2022-10-04 10:59:07 +0200

    hamburger menu is back

--- frontend/script.js
+++ frontend/script.js
@@ -3,13 +3,11 @@ const rootElement = document.querySelector("#root");
 rootElement.insertAdjacentHTML("afterend", 
 `<div class="container">
     <div class="nav">
-        <a href="">Hamburger</a>
-        <a href="">apartments</a>
-        <a href="">why griffith wood</a>
-        <a href="">living here</a>
-        <a href="">LOGO</a>,
-        <a href="">find us</a>
-        <a href="">enquire now</a>
+    <a href="">
+    <div class="hamburger-icon"></div>
+    <div class="hamburger-icon"></div>
+    <div class="hamburger-icon"></div></a>
+    <a href="">apartments</a><a href="">why griffith wood</a><a href="">living here</a><a href="">LOGO</a><a href="">find us</a><a href="">enquire now</a>
     </div>
     <div class="titleText">
         <h1>A new way of city living</h1>
